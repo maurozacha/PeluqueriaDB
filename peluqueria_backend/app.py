15f5bddf0138dabdaf8f0f9ca256a88b13815246
mzacharczuk <mzacharczuk@senasa.gov.ar>
2025-06-12 03:22:24 +0000

    se agrega configuracion al dockerfile para que descargue pytest, se agrega en el setting,json la configuracion de la bd

--- peluqueria_backend/app.py
+++ peluqueria_backend/app.py
@@ -1,21 +1,12 @@
 from flask import Flask
-from config import Config
-from src.extensions import db
-from flask_migrate import Migrate
-from models.cliente import Cliente
-from models.servicio import Servicio
-from models.turno import Turno
+from extensions import db
+from routes.cliente_routes import cliente_bp
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config.from_object('config.Config')
 db.init_app(app)
-migrate = Migrate(app, db)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-from routes import appointments, clients
+app.register_blueprint(cliente_bp)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
